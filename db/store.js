4e0e5572d3ee5fc85a2dc703eb206f170d1f82f4
Skoobieton <91810317+Tanyaskubon@users.noreply.github.com>
2022-02-08 18:48:48 -0500

    it works

--- db/store.js
+++ db/store.js
@@ -4,27 +4,38 @@ const { v4: uuidv4 } = require('uuid');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 class Store {
-    read () {
+    read() {
         return readFileAsync('db/db.json', 'utf-8');
     }
-    write (note) {
+    write(note) {
         return writeFileAsync('db/db.json', JSON.stringify(note))
     }
-    getNotes(id, note) {
-    const result = note.filter((note) => note.id === id)[0];
-        return result;
-        }
-    
-    addNotes(body,note) {
-    const note = body;
-   note.push(note);
-   fs.writeFileSync(
-       path.join(__dirname, "../db/note.json"),
-       JSON.stringify({ note }, null, 2)
-   );
-    const newNote = { title, text, id: uuidv4() };
-   };
-};
-    
+    getNotes() {
+        return this.read().then((notes) => {
+            let newNote;
+            try {
+                newNote = [].concat(JSON.parse(notes))
+            } catch (error) {
+                newNote = []   
+            }
+            return newNote;
+        })
+    }
 
-module.exports =  Store
+    addNotes(note) {
+        const {title, text} = note;
+       
+        const newNote = { title, text, id: uuidv4() };
+        return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((info) => this.write(info))
+        .then(() => newNote)
+    };
+    deleteNotes (){
+        return this.getNotes().then((notes)=> notes.filter((note)=> note.id !== id))
+        .then((notesFiltered) => this.write(notesFiltered))
+    }
+};
+
+
+module.exports = new Store()
